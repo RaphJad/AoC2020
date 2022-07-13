248ee920f7d0c37b21860376a502b6f3f61bf8f8
RaphJad <21295@ecam.be>
2022-07-13 18:01:07 +0200

    Add day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,19 +1,38 @@
 import numpy as np
 #read the input
 set_data = open("input.txt").readlines()
-numbers, policy, password = [[]], [], []
+numbers, policy, password = [], [], []
+for line in set_data:
+    split = line.split(' ')
+    min, max = int(split[0].split('-')[0]), int(split[0].split('-')[1])
+    numbers.append([min,max])
+    policy.append(split[1][0])
+    password.append(split[2][0:len(split[2])-1])
 
-for i in range(len(set_data)):
-    data = set_data[i].split()
-    numbers[i]
+#part 1
+count = 0
+for i in range(len(numbers)):
+    if password[i].count(policy[i]) >= numbers[i][0] and password[i].count(policy[i]) <= numbers[i][1]:
+        count += 1
+print(count)
 
-for data in set_data:
-    print(data)
-    
-    #split the data into numbers and policy
-    numbers.append(data.split("-")[0])
-    policy.append(data.split("-")[1])
-    password.append(data.split("-")[2])
-    print(numbers)
-    print(policy)
-    print(password)
+
+
+#part 2
+count = 0
+for i in range(len(numbers)):
+    if(password[i][numbers[i][0]-1] == policy[i]):
+        if(len(password[i]) >= numbers[i][1]):
+            if(password[i][numbers[i][1]-1] != policy[i]):
+                print(password[i][numbers[i][0]-1], password[i][numbers[i][1]-1])
+                count += 1
+            else:
+                continue
+    else:
+        if(len(password[i]) >= numbers[i][1]):
+            if(password[i][numbers[i][1]-1] == policy[i]):
+                print(password[i][numbers[i][0]-1], password[i][numbers[i][1]-1])
+                count += 1
+            else:
+                continue
+print(count)
